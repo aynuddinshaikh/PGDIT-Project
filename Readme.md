ed647bbec1fc44bd2ba2f1522d3676c232d0f7b5
Aynuddin Shaikh <aynuddinshaikh180@gmail.com>
2024-08-19 20:07:29 +0530

    final updated

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# PGDIT-Project
---
+++ src/main/java/com/mycompany/pgditproject/Pgditproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pgditproject;
+
+/**
+ *
+ * @author HOME
+ */
+public class Pgditproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
